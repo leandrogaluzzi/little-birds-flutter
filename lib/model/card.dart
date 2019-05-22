2e68146f42caf38e30f0f09479cfb5301a1341b8
leandrogaluzzi <leandrogaluzzi@gmail.com>
2019-05-23 00:26:23 +0200

    Card initial implementation

--- lib/model/card.dart
+++ lib/model/card.dart
@@ -1,1 +1,31 @@
-class CardItem {}
+class CardItem {
+  CardItem({
+    this.packCode,
+    this.packName,
+    this.typeName,
+    this.factionName,
+    this.code,
+    this.name,
+    this.cost,
+    this.text,
+  });
+
+  final String packCode;
+  final String packName;
+  final String typeName;
+  final String factionName;
+  final String code;
+  final String name;
+  final int cost;
+  final String text;
+
+  CardItem.fromJson(Map<String, dynamic> json)
+      : packCode = json['pack_code'],
+        packName = json['pack_name'],
+        typeName = json['type_name'],
+        factionName = json['faction_name'],
+        code = json['code'],
+        name = json['name'],
+        cost = json['cost'],
+        text = json['text'];
+}
