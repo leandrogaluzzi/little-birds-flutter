1a86297c0fad046f384d6d8a2811e61537ab603a
leandrogaluzzi <leandrogaluzzi@gmail.com>
2019-05-24 00:37:25 +0200

    Implementation of getPacks

--- lib/networking/thrones_api.dart
+++ lib/networking/thrones_api.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'network_provider.dart';
 import 'package:little_birds/model/card.dart';
+import 'package:little_birds/model/pack.dart';
 import 'dart:convert';
 
 const baseURL = 'https://thronesdb.com';
-const allCardsURL = '/api/public/cards/';
+const _cardsURL = '/api/public/cards/';
+const _packsURL = '/api/public/packs/';
 
 class ThronesException extends Exception {
   factory ThronesException([var message]) => ThronesException(message);
@@ -17,13 +19,25 @@ class ThronesAPI {
 
   final NetworkProvider network;
 
-  Future<List<CardItem>> getAllCards() async {
+  Future<List<CardItem>> getCards() async {
     try {
-      String response = await network.get(allCardsURL);
+      String response = await network.get(_cardsURL);
       List<dynamic> list = await json.decode(response);
       List<CardItem> cards =
           list.map((item) => CardItem.fromJson(item)).toList();
       return cards;
+    } catch (e) {
+      throw ThronesException(e);
+    }
+  }
+
+  Future<List<Pack>> getPacks() async {
+    try {
+      String response = await network.get(_packsURL);
+      List<dynamic> list = await json.decode(response);
+      List<Pack> packs =
+          list.map((item) => Pack.fromJson(item)).toList();
+      return packs;
     } catch (e) {
       throw ThronesException(e);
     }
