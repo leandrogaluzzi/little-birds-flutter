201fe3d8e4a911134ce1c288f3636519ae138840
leandrogaluzzi <leandrogaluzzi@gmail.com>
2019-08-16 15:41:43 +0200

    MainScreen layout initial implementation

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:little_birds/cards_store.dart';
 import 'package:little_birds/networking/thrones_service.dart';
 import 'package:little_birds/model/thrones_card.dart';
+import 'package:little_birds/utils/constants.dart';
 import 'package:little_birds/widgets/tab_bar_component.dart';
 
 class MainScreen extends StatefulWidget {
@@ -10,26 +11,83 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  ThronesService _thronesService = ThronesService();
   Future _cards;
 
   @override
   void initState() {
-    ThronesService thronesService = ThronesService();
-    _cards = thronesService.getCards();
+    _cards = _thronesService.getCards();
     super.initState();
   }
 
   Widget _widgetLoading() {
-    return Center(
-      child: CircularProgressIndicator(),
+    return Scaffold(
+      body: Container(
+        color: kColorYellowLittleBirds,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Loading cards ...',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+            Container(
+              height: 100,
+              child: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   Widget _widgetError({Error error}) {
-    return Container(color: Colors.red);
+    return Scaffold(
+      body: Container(
+        color: kColorYellowLittleBirds,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Error loading cards.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+            Container(height: 12.0),
+            Text(
+              'Please, check your internet connection',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14),
+            ),
+            Container(
+              height: 100,
+              child: Center(
+                child: RaisedButton(
+                  color: Colors.grey[300],
+                  child:
+                      Text('Try again', style: TextStyle(color: Colors.black)),
+                  onPressed: () {
+                    setState(() {
+                      _cards = _thronesService.getCards();
+                    });
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
-  Widget _widgetTabs({ @required List<ThronesCard> cards}) {
+  Widget _widgetTabs({@required List<ThronesCard> cards}) {
     return CardsStore(
       cards: cards,
       child: TabBarComponent(),
@@ -38,21 +96,22 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<ThronesCard>>(
+    return _widgetLoading();
+    /*return FutureBuilder<List<ThronesCard>>(
       future: _cards,
-      builder: (BuildContext context, AsyncSnapshot<List<ThronesCard>> snapshot) {
+      builder:
+          (BuildContext context, AsyncSnapshot<List<ThronesCard>> snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
           case ConnectionState.active:
           case ConnectionState.waiting:
             return _widgetLoading();
           case ConnectionState.done:
-            if (snapshot.hasError) 
-              return _widgetError(error: snapshot.error); 
+            if (snapshot.hasError) return _widgetError(error: snapshot.error);
             return _widgetTabs(cards: snapshot.data);
         }
         return null;
       },
-    );
+    );*/
   }
 }
