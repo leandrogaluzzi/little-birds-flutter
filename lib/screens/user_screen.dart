32a890a915d1aa1cebe6895369695ee39d3921c9
leandrogaluzzi <leandrogaluzzi@gmail.com>
2019-09-14 18:09:13 +0200

    Getting code from authentication

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:little_birds/api/thrones_constants.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:uni_links/uni_links.dart';
 
 class UserScreen extends StatefulWidget {
   @override
@@ -8,11 +10,33 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  StreamSubscription _sub;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _sub = getUriLinksStream().listen((Uri uri) {
+      String code = uri.queryParameters['code'];
+      print(code);
+      // Parse the link and warn the user, if it is not correct
+    }, onError: (err) {
+      print(err);
+      // Handle exception by warning the user their action did not succeed
+    });
+  }
+
+  @override
+  void dispose() {
+    _sub.cancel();
+    super.dispose();
+  }
+
   _openAuth() async {
     final url = ThronesConstants.authURL();
 
     if (await canLaunch(url)) {
-      await launch(url);
+      await launch(url, forceSafariVC: false);
     }
   }
 
